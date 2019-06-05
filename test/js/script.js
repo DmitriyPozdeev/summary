cd9fd8158c587cbb9f51097a6e3b9cf79a21f354
Dmitriy Pozdeev <dmitriyweb@inbox.ru>
2019-06-05 16:55:34 +0500

    add logic create LE and scope work

--- test/js/script.js
+++ test/js/script.js
@@ -14,7 +14,7 @@
 
   return 1
  };
- console.log(smallestDivisor(10));//smallestDivisor(1); */
+ console.log(smallestDivisor(10));//smallestDivisor(1); 
  
  //Test scope
 
@@ -22,13 +22,96 @@ function outer() {
   var x = 1,
       y = 5,
       z = 10;
-    return function() {
+    return (function() {
       return z-- 
-    }
+    })();
 };
-var variable0 = outer();
+var variable0 = outer;
 console.log(variable0());
 
-console.log(variable0());
 
-console.log(variable0());
+
+var a = 1;
+
+function getFunc() {
+  var a = 2;
+
+  var func = new Function('', 'alert(a)');
+
+  return func();
+}
+
+getFunc(); 
+
+*/
+function sum(a) {
+  return function(b) {
+    return a + b;
+  }
+}
+console.log(sum(5))
+/*
+function makeBuffer() {
+  var result = '';
+  return function buffer( newValue ){
+    if(typeof(newValue) === 'undefined'){
+      return result
+    }
+    buffer.clear = function(){
+      result = '';
+    }
+    result += newValue;
+  }
+}
+var buffer = makeBuffer();
+
+buffer("Тест");
+buffer(" тебя не съест ");
+alert( buffer() ); // Тест тебя не съест
+
+buffer.clear();
+
+alert( buffer() ); // ""
+
+var users = [{
+  name: "Вася",
+  surname: 'Иванов',
+  age: 20
+}, {
+  name: "Петя",
+  surname: 'Чапаев',
+  age: 25
+}, {
+  name: "Маша",
+  surname: 'Медведева',
+  age: 18
+}];
+
+/*users.sort(function(a, b) {
+  return a.age > b.age ? 1 : -1;
+});
+console.log(users)
+
+function byField(field) {
+  return function(a, b) {
+    return a[field] > b[field] ? 1 : -1;
+  }
+}
+users.sort(byField('age'));
+users.forEach(function(user) {
+  alert( user.name );
+});
+var arr = [1, 2, 3, 4, 5, 6, 7];
+function filter(arr, func) {
+  return arr.filter(func);
+}
+
+function inBetween(a,b) {
+  return arr.filter(function(c){
+    return (c >= a) && (c <= b);
+  })
+}
+alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
+/*alert(filter(arr, function(a) {
+  return a % 2 == 0
+})); // 2,4,6*/
