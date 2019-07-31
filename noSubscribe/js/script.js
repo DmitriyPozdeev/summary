41655ac233517d2aea7a2965bf89a7b64e7666b9
Dmitriy Pozdeev <dmitriyweb@inbox.ru>
2019-07-31 16:52:14 +0500

    continue noSubscribe

--- noSubscribe/js/script.js
+++ noSubscribe/js/script.js
@@ -52,4 +52,93 @@ const sumSquareDifference = (n) => {
   return iter(0, 0, 0);
 };
 console.log(sumSquareDifference(10));
+
+Реализуйте и экспортируйте по умолчанию каррированую версию функции, которая принимает на вход
+три строки и возвращает новую строчку составленную из трех входных.
+
+// исходная версия
+concat('a', 'bc', 'f'); // => abcf
+
+// каррированая версия
+concat('a')('bc')('f'); // => abcf
+
+const concat = (x) => (y) => (z) => `${x}${y}${z}`;
+console.log(concat('a')('bc')('f'));
+
+
+С точки зрения математики, композиция функций f и g - новая функция z = f(g(x)).
+
+compose.js
+Реализуйте и экспортируйте по умолчанию функцию compose, которая принимает на вход две других одноаргументных функции и возвращает новую функцию. 
+Эта новая функция также принимает на вход один параметр и при вызове применяет его последовательно к переданным функциям в обратном порядке.
+
+Примеры ниже помогут понять, как должна работать функция:
+
+const f = compose(Math.sqrt, Math.abs);
+f(-4); // => 2
+
+compose(v => v, v => v)(10); // => 10
+compose(v => v + 2, v => v)(10); // => 12
+compose(v => v, v => v - 2)(10); // => 8
+compose(v => v ** 2, v => v - 2)(2); // => 0
+compose(v => v - 2, v => v ** 2)(2); // => 2
+
+const compose = (f1, f2) => f3 = (x) => f1(f2(x));
+
+
+
+
+Реализуйте и экспортируйте по умолчанию функцию isPowerOfThree, которая определяет, является ли переданное число натуральной степенью тройки.
+ Например, число 27 — это третья степень: 3^3, а 81 — это четвёртая: 3^4.
+
+Пример:
+
+isPowerOfThree(1); // true (3^0)
+isPowerOfThree(2); // false
+isPowerOfThree(9); // true (3^2)
+
+const isPowerOfThree = (num) => {
+  const iter = (acc) => {
+    if (acc !== Math.round(acc)) return false;
+    if (acc === 1) return true;
+    return iter(acc / 3);
+  };
+  return iter(num)
+
+}
+console.log(isPowerOfThree(720));
+
+Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех.
+
+Например, билет с номером 385916 - счастливый, так как 3 + 8 + 5 = 9 + 1 + 6
+
+isHappyTicket.js
+Напишите и экспортируйте по умолчанию функцию, проверяющую является ли номер счастливым
+(номер может быть как числового, так и строкового типа: см. примеры ниже). 
+Функция должна возвращать true, если билет счастливый, или false, если нет.
+
+Примеры использования:
+
+import isHappyTicket from './isHappyTicket';
+
+isHappyTicket(385916); // true
+isHappyTicket(231002); // false
+isHappyTicket(128722); // true
+isHappyTicket('054702'); // true
+
+const isHappyTicket = (num) => {
+  num = num + '';
+  let first = 0;
+  let second = 0;
+  for (let i = 0; i < num.length / 2; i += 1) {
+    first += +num[i];
+  }
+  for (let i = num.length / 2; i < num.length; i += 1) {
+    second += +num[i];
+  }
+  return first === second ? true : false;
+};
+console.log(isHappyTicket(385916));
+
+
 */
